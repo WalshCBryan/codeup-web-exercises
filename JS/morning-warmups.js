4b68747dd12510f705989c6f0be6ec024db42865
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-10-09 10:46:54 -0500

    conditional lecture w vivian

--- JS/morning-warmups.js
+++ JS/morning-warmups.js
@@ -149,10 +149,7 @@ function greaterThanfive() {
 
 function heyStranger(name= "stranger" ) {
     var name = prompt("What's your name??");
-     alert("Hey there, " + name); {
-         if name = undefined; {
-             alert ("Hey there, stranger")}
-    }
+     alert("Hey there, " + name);
 }
 
 // if no name is passed, should alert "hey there, stranger" but instead alerts "hey there, _______"
