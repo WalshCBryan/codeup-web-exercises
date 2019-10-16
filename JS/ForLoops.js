e71dc529e297339d1771c008a133dc5818295c44
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-10-15 21:44:58 -0500

    working with loops

---
+++ JS/ForLoops.js
@@ -0,0 +1,75 @@
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+// var i = 0;
+//
+// function showMultiplictionTable(num) {
+//     while (i <= 10) {
+//         console.log("" + num + " x " + i + " = " + (num * i));
+//         i++;
+//     }
+// } return(console.log("All done"));
+
+function showMultiplicationTable(num) {
+    for (i = 0; i <= 10; i++) {
+        console.log("" + num + " x " + i + " = " + (num * i));
+
+    } return(console.log("All done"));
+}
+
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+
+
+// Math.floor(Math.random() * 50) + 1;
+//
+// function randomOdds() {
+//     for (i = 0; i <= 20; i++) {
+//         if (Math.floor(Math.random() * 50) + 1;) % 2 === 0) {
+//         }
+//
+//     }
+//
+// }
+
+function randomOdds() {
+    for (var x = 0; x <= 20; x++) {
+        randonNum = Math.floor(Math.random() * 50) + 1;
+        if (randonNum % 2 === 0) {
+            console.log(randonNum + " is even");
+        } else {
+            console.log(randonNum + " is odd");
+        }
+    }
+}
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+// function numberPyramid() {
+//     for (var x = 1; x <= 9; x++) {
+//         console.log("" + x)
+//     }
+// }
+
+function numberPyramid() {
+    var num = "";
+    for (var i = 1; i <= 9; i++) {
+        num += i;
+        console.log(num);
+    }
+}
+//how to get it to repeat same number instead of iterated number?
+
+function hundredToFive() {
+    for (x = 100; x >= 5; x -= 5) {
+        console.log(x);
+    }
+
+}
--- JS/While.js
+++ JS/While.js
@@ -11,7 +11,40 @@ var i = 1;
 // This is how you get a random number between 50 and 100n(outside of loop)
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5 (inside do-while loop)
-var coneSale = Math.floor(Math.random() * 5) + 1;
+// var coneSale = Math.floor(Math.random() * 5) + 1;
+
+//    5 cones sold...  // if there are enough cones
+//    Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+//    Yay! I sold them all! // If there are no more cones
 
 //tracks allcones-conessold
+
+// while (allCones > 0 && allCones > coneSale){
+//     allCones -= coneSale;
+//     console.log("you sold " + coneSale + " cones.");
+// }
+
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+// do {
+//     var coneSale = Math.floor(Math.random() * 5) + 1;
+//     console.log("you sold " + coneSale + " cones.");
+//     allCones -= coneSale;
+// }
+// while (allCones > 0);
+
+
+do {
+    var coneSale = Math.floor(Math.random() * 5) + 1;
+    allCones -= coneSale;
+    console.log("You sold " + coneSale + " cones.");
+    if (coneSale > allCones)
+    {console.log("You cannot sell " + coneSale + ", You only have " + allCones + " left.")
+    }
+
+} while (allCones > 0);
+console.log("You sold all of your cones!!");
+
 
