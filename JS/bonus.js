1296bad4868a664a149f13c901e177718c2ba59f
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-10-21 15:15:36 -0500

    working wiht intervals and timeouts

--- JS/bonus.js
+++ JS/bonus.js
@@ -21,6 +21,8 @@ function canJoin() {
 //
 // Rodrigo needs to buy tofu, lamb, and potatoes.
 //
+// Return a string to Rodrigo with his total based on how much of each item he's buying, by weight in pounds, the price of the
+// items per pound, and also inform him of the savings he's receiving from the store.
 
 var tofuPrice = 4.99;
 var lambPrice = 14.99;
@@ -37,5 +39,135 @@ function purchaseOrder(x,y,z) {
     return ("You are buying " + x + " lb(s) of tofu at $" + tofuPrice + " per lb. You are buying " + y + " lb(s) of lamb at $" + lambPrice + " per lb. You are buying " + z + " lb(s) of potatoes at $" + potatoPrice + " per lb. Your pre discount total is $" + total + ". Your " + discountRate + "% discount gets you an additional $" + (total * (discountRate / 100)).toFixed(2) + ". Your new final total is $" + (total -(total * (discountRate / 100))).toFixed(2));
 
 }
-//Return a string to Rodrigo with his total based on how much of each item he's buying, by weight in pounds, the price of the
-// items per pound, and also inform him of the savings he's receiving from the store.
+
+// Write a function, filterNumbers() that takes in an array of mixed data types and returns an array of only the numbers type in ascencding order.
+//
+//     Example input: ["fred", true, 5, 3] Example output: [3, 5]
+
+function filterNumbers(x){
+    var newArray = [];
+    for (i = 0; i <= x.length; i++) {
+        if (typeof(x[i]) === "number") {
+            // console.log(typeof (x[i]));
+            newArray.unshift(parseFloat(x[i]));
+        }
+    }
+    var newNewArray = newArray.sort(function(a,b) {
+        return a-b;
+    });
+    return newNewArray;
+}
+
+// Write a function, getOlder() that takes in array of dog objects and increases the value of each object's age property by 1.
+//
+// Example input:
+
+var doggos = [
+    {
+        name: "Chompers",
+        breed: "Pug",
+        age: 7
+    },
+    {
+        name: "Freddy",
+        breed: "Lab",
+        age: 4
+    },
+    {
+        name: "Mr. Pig",
+        breed: "Mastif",
+        age: 10
+    }
+];
+
+console.log(doggos);
+
+function addDogYear(doggos) {
+    doggos.forEach(function(doggo,index) {
+        doggos[index].age += 1
+    });
+    console.log(doggos);
+}
+
+// function addDogYear() {
+//     for (var i = 0; i <= doggos.length; i++) {
+//         this.doggos.age += 1
+//     } console.log(doggos);
+//
+// }
+
+var cars = [
+    {
+        make: 'Volvo',
+        color: 'red',
+        year: 1996,
+        isDirty: true
+    },
+    {
+        make: 'Toyota',
+        color: 'black',
+        year: 2004,
+        isDirty: false
+    },
+    {
+        make: 'Ford',
+        color: 'white',
+        year: 2007,
+        isDirty: true
+    }
+];
+
+console.log(cars);
+
+function washCars(cars) {
+    cars.forEach(function(car,index) {
+        cars[index].isDirty = false
+    });
+    console.log(cars);
+}
+
+// Write a function, adminList() that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+
+var credentials = [
+    {
+        isAdmin: true,
+        email: 'user1@email.com'
+    },
+    {
+        isAdmin: true,
+        email: 'user2@email.com'
+    },
+    {
+        isAdmin: false,
+        email: 'user3@email.com'
+    }
+];
+
+console.log(credentials);
+
+
+function adminCount(credentials) {
+    var admins = [];
+    credentials.forEach(function(credential,index) {
+       if (credentials[index].isAdmin === true) {
+           admins.unshift(credential.email);
+       }
+    });
+    console.log(admins);
+}
+
+// Refactor to return an array of admin-only user emails
+
+function adminEmails(credentials) {
+    var admins = [];
+    credentials.forEach(function(credential,index) {
+        if (credentials[index].isAdmin === true) {
+            admins.unshift(credential);
+        }
+    });
+    console.log(admins);
+}
+
+
+
+
