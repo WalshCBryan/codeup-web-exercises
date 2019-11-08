9774aeaebf9c2cd190c803cc2111be3942345ef7
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-11-07 18:23:56 -0600

    weathermap project

--- JS/mapbox-geocoder-utils.js
+++ JS/mapbox-geocoder-utils.js
@@ -39,6 +39,21 @@ function geocode(search, token) {
  *  })
  *
  */
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return (data.features[0]);
+//         });
+//
+// }
+
+
 function reverseGeocode(coordinates, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
@@ -48,6 +63,12 @@ function reverseGeocode(coordinates, token) {
         })
         // to get all the data from the request, comment out the following three lines...
         .then(function(data) {
-            return data.features[0].place_name;
-        });
+            console.log(data.features[1].place_name);
+            if (data.features !== undefined) {
+                return data.features[1].place_name;
+            } else return ("Information not available");
+        })
+
 }
+
+
